9c71d53e8e25d5f949dfc7c86e5c1fc2fdc7963c
mjkeaton <michaeljkeaton@protonmail.com>
2025-02-24 17:05:00 +0100

    chore: remove utils/api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,30 @@
+import { API_URL } from "@/constants/api"
 import { BALANCES, INFO } from "@/constants/endpoints"
-import { apiFetch } from "@/utils/api"
+
+const apiFetch = async <T = unknown>(endpoint: string, options: RequestInit = {}): Promise<T> => {
+  const url = `${API_URL}${endpoint}`
+
+  const response = await fetch(url, {
+    ...options,
+    /* headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    }, */
+    headers: options.headers ?? [],
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.statusText}`)
+  }
+
+  const contentLength = response.headers.get("Content-Length")
+
+  if (contentLength === "0" || response.headers.get("Content-Type")?.includes("application/json") === false) {
+    return {} as T
+  }
+
+  return response.json() as Promise<T>
+}
 
 export interface InfoResponse {
   name?: string
