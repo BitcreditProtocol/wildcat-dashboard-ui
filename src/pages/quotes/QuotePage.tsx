c282424ac17778233e5b57a2c932ef7a5c291e87
mjkeaton <michaeljkeaton@protonmail.com>
2025-02-25 12:07:51 +0100

    feat: add deny and offer actions

--- src/pages/quotes/QuotePage.tsx
+++ src/pages/quotes/QuotePage.tsx
@@ -1,11 +1,16 @@
 import { Breadcrumbs } from "@/components/Breadcrumbs"
 import { PageTitle } from "@/components/PageTitle"
+import { Button } from "@/components/ui/button"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
 import { InfoReply } from "@/generated/client"
-import { adminLookupQuoteOptions } from "@/generated/client/@tanstack/react-query.gen"
+import {
+  adminLookupQuoteOptions,
+  adminLookupQuoteQueryKey,
+  resolveQuoteMutation,
+} from "@/generated/client/@tanstack/react-query.gen"
 import useLocalStorage from "@/hooks/use-local-storage"
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query"
 import { Suspense } from "react"
 import { Link, useParams } from "react-router"
 
@@ -14,6 +19,64 @@ function Loader() {
     <div className="flex flex-col gap-1.5 py-2">
       <Skeleton className="h-48 rounded-lg" />
     </div>
+  )
+}
+
+function QuoteActions({ value }: { value: InfoReply }) {
+  const queryClient = useQueryClient()
+
+  const resolveQuote = useMutation({
+    ...resolveQuoteMutation(),
+    onError: (error) => {
+      console.log(error)
+    },
+    onSuccess: (data) => {
+      console.log(data)
+      void queryClient.invalidateQueries({
+        queryKey: adminLookupQuoteQueryKey({
+          path: {
+            id: value.id,
+          },
+        }),
+      })
+    },
+  })
+
+  const onDenyQuote = () => {
+    resolveQuote.mutate({
+      path: {
+        id: value.id,
+      },
+      body: {
+        action: "deny",
+      },
+    })
+  }
+
+  const onOfferQuote = () => {
+    resolveQuote.mutate({
+      path: {
+        id: value.id,
+      },
+      body: {
+        action: "offer",
+        discount: "1",
+        ttl: "1",
+      },
+    })
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-2">
+        <Button className="flex-1" onClick={onDenyQuote} disabled={value.status === "denied"}>
+          Deny
+        </Button>
+        <Button className="flex-1" onClick={onOfferQuote} disabled={value.status === "offered"}>
+          Offer
+        </Button>
+      </div>
+    </>
   )
 }
 
@@ -32,15 +95,13 @@ function Quote({ value }: { value: InfoReply }) {
               <TableCell>{value.status}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell>endorser: </TableCell>
-              <TableCell>{value.endorser || "(empty)"}</TableCell>
-            </TableRow>
-            <TableRow>
               <TableCell>bill: </TableCell>
               <TableCell>{value.bill || "(empty)"}</TableCell>
             </TableRow>
           </TableBody>
         </Table>
+
+        <QuoteActions value={value} />
       </div>
     </>
   )
