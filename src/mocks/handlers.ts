56ae16c988c45e7ccf00e37bd586bc077969dfce
mjkeaton <michaeljkeaton@protonmail.com>
2025-02-25 12:07:51 +0100

    feat: add deny and offer actions

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,5 @@
-import { fetchAdminLookupQuote, fetchAdminQuotePending } from "./handlers/admin_quotes"
+import { fetchAdminLookupQuote, fetchAdminQuotePending, updateAdminQuote } from "./handlers/admin_quotes"
 import { fetchBalances } from "./handlers/balances"
 import { fetchInfo } from "./handlers/info"
 
-export const handlers = [fetchInfo, fetchBalances, fetchAdminQuotePending, fetchAdminLookupQuote]
+export const handlers = [fetchInfo, fetchBalances, fetchAdminQuotePending, fetchAdminLookupQuote, updateAdminQuote]
--- src/mocks/handlers/admin_quotes.ts
+++ src/mocks/handlers/admin_quotes.ts
@@ -1,7 +1,7 @@
 import { http, delay, HttpResponse, StrictResponse } from "msw"
 import { API_URL } from "@/constants/api"
 import { ADMIN_QUOTE_BY_ID, ADMIN_QUOTE_PENDING } from "@/constants/endpoints"
-import { AdminLookupQuoteResponse, InfoReply, ListPendingQuotesResponse } from "@/generated/client"
+import { AdminLookupQuoteResponse, InfoReply, ListPendingQuotesResponse, ResolveRequest } from "@/generated/client"
 import { db } from "../db"
 
 export const fetchAdminQuotePending = http.get<never, never, ListPendingQuotesResponse>(
@@ -25,11 +25,42 @@ export const fetchAdminLookupQuote = http.get<never, never, AdminLookupQuoteResp
     await delay(1_000)
 
     const data = db.quotes.getAll().filter((it) => it.id === id)
-
     if (data.length === 0) {
       return HttpResponse.json(null, { status: 404 }) as unknown as StrictResponse<InfoReply>
     }
 
     return HttpResponse.json(data[0] as InfoReply)
+  },
+)
+
+export const updateAdminQuote = http.post<never, ResolveRequest>(
+  `${API_URL}${ADMIN_QUOTE_BY_ID}`,
+  async ({ params, request }) => {
+    const { id } = params
+    const body = await request.json()
+
+    await delay(1_000)
+
+    const data = db.quotes.getAll().filter((it) => it.id === id)
+    if (data.length === 0) {
+      return HttpResponse.json(null, { status: 404 }) as unknown as StrictResponse<InfoReply>
+    }
+
+    const quote = data[0]
+
+    if (body.action === "deny") {
+      quote.status = "denied"
+    }
+    if (body.action === "offer") {
+      quote.status = "offered"
+      quote.ttl = body.ttl ?? null
+    }
+
+    const updated = db.quotes.update({
+      where: { id: { equals: quote.id } },
+      data: quote,
+    })
+
+    return HttpResponse.json(updated as InfoReply)
   },
 )
