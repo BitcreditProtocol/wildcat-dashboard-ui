88724065830cb1f92f9d2c8915c8694882d25cad
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-10 21:54:36 +0100

    chore: navbar secondary nav

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,48 +1,65 @@
-import { Bitcoin, Home, Inbox, Settings, InfoIcon } from "lucide-react"
+import { Bitcoin, Home, Inbox, Settings, InfoIcon, LifeBuoy, Send } from "lucide-react"
 import { Sidebar, SidebarContent, SidebarFooter, SidebarRail } from "@/components/ui/sidebar"
 import { NavUser } from "./nav/NavUser"
 import { randomAvatar } from "@/utils/dev"
 import { NavMain } from "./nav/NavMain"
+import { NavSecondary } from "./nav/NavSecondary"
 
-const items = [
-  {
-    title: "Home",
-    url: "/",
-    icon: Home,
-  },
-  {
-    title: "Balances",
-    url: "/balances",
-    icon: Bitcoin,
-  },
-  {
-    title: "Quotes",
-    url: "/quotes",
-    icon: Inbox,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-    items: [
-      {
-        title: "General",
-        url: "/settings",
-      },
-    ],
-  },
-  {
-    title: "Info",
-    url: "/info",
-    icon: InfoIcon,
-  },
-]
+const data = {
+  navMain: [
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Balances",
+      url: "/balances",
+      icon: Bitcoin,
+    },
+    {
+      title: "Quotes",
+      url: "/quotes",
+      icon: Inbox,
+    },
+    {
+      title: "Settings",
+      url: "/#",
+      icon: Settings,
+      items: [
+        {
+          title: "General",
+          url: "/settings",
+        },
+      ],
+    },
+    {
+      title: "Info",
+      url: "/info",
+      icon: InfoIcon,
+    },
+  ],
+
+  navSecondary: [
+    {
+      title: "Support",
+      url: "/#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "/#",
+      icon: Send,
+    },
+  ],
+}
 
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
       <SidebarContent>
-        <NavMain items={items} />
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
         <NavUser
