cdc530e9488bbe229095a33f2e4cf8d585fe7aee
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-12 10:24:24 +0100

    chore: prepare Earnings component

--- src/pages/balances/BalancesPage.tsx
+++ src/pages/balances/BalancesPage.tsx
@@ -3,11 +3,12 @@ import { PageTitle } from "@/components/PageTitle"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
 import { type ChartConfig, ChartContainer, ChartLegend, ChartLegendContent } from "@/components/ui/chart"
-import { PropsWithChildren, Suspense } from "react"
+import { PropsWithChildren, Suspense, useState } from "react"
 import { Skeleton } from "@/components/ui/skeleton"
 import { BalancesResponse, fetchBalances } from "@/lib/api"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import useLocalStorage from "@/hooks/use-local-storage"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
 
 function Loader() {
   return (
@@ -125,6 +126,57 @@ export function BalanceText({ value, children }: PropsWithChildren<{ value: Bala
   )
 }
 
+function Earnings() {
+  const [timeframe, setTimeframe] = useState("1d")
+
+  return (
+    <div className="hidden">
+      <div className="flex flex-col justify-center gap-2 my-4 mt-8">
+        <div className="flex justify-center gap-2 my-1">
+          <div className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">0.00 000 000 BTC</div>
+        </div>
+        <div className="flex justify-center gap-2">
+          <div className="scroll-m-20 tracking-tight text-sm">Earned during the selected timeframe</div>
+        </div>
+      </div>
+
+      <div className="flex justify-center gap-2 my-4">
+        <ToggleGroup
+          type="single"
+          size="lg"
+          variant="outline"
+          value={timeframe}
+          onValueChange={(val) => setTimeframe((curr) => val || curr)}
+        >
+          <ToggleGroupItem value="1d" className="px-6 whitespace-nowrap">
+            Today
+          </ToggleGroupItem>
+          <ToggleGroupItem value="1w" className="px-6 whitespace-nowrap">
+            Last week
+          </ToggleGroupItem>
+          <ToggleGroupItem value="1m" className="px-6 whitespace-nowrap">
+            Last month
+          </ToggleGroupItem>
+          <ToggleGroupItem value="3m" className="px-6 whitespace-nowrap">
+            Last 3 months
+          </ToggleGroupItem>
+          <ToggleGroupItem value="6m" className="px-6 whitespace-nowrap">
+            Last 6 months
+          </ToggleGroupItem>
+          <ToggleGroupItem value="1y" className="px-6 whitespace-nowrap">
+            Last year
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+      <div className="flex flex-col justify-center gap-2 my-4 mt-8">
+        <div className="flex justify-center gap-2">
+          <div className="scroll-m-20 tracking-tight text-sm">No accepted quotes for the selected timeframe.</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function PageBody() {
   const { data } = useSuspenseQuery({
     queryKey: ["balances"],
@@ -167,6 +219,8 @@ function PageBody() {
           </CardContent>
         </Card>
       </div>
+
+      <Earnings />
 
       <div className="flex items-center gap-2 hidden">
         <Card className="flex-1 max-h-[500px] py-4">
