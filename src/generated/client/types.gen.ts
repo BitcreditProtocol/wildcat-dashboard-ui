d10dff2d0efe5fb978539c1d4f71fcb8aefb4b99
JulianVIE <julian.hiess@gmail.com>
2026-02-04 14:46:56 +0100

    #189 update openapi

--- src/generated/client/types.gen.ts
+++ src/generated/client/types.gen.ts
@@ -1218,7 +1218,9 @@ export type GetSatBalanceResponse = GetSatBalanceResponses[keyof GetSatBalanceRe
 
 export type GetEbillMintCompleteData = {
     body?: never;
-    path?: never;
+    path: {
+        bid: string;
+    };
     query?: never;
     url: '/v1/admin/treasury/ebill/payment_complete/{bid}';
 };
