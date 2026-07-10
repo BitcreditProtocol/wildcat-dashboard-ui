d23c2cda9efe7fc618d59806216b7c45e2220d0b
JulianVIE <julian.hiess@gmail.com>
2026-07-10 13:32:43 +0200

    #196 update pr

--- src/context/language/LanguageContext.ts
+++ src/context/language/LanguageContext.ts
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import meta from "@/constants/meta";
+import { LIVE_TRANSLATIONS_KEY } from "@/constants/storageKeys";
 
 /**
  * Crowdin "pseudo-language" for In-Context tooling
@@ -9,8 +10,11 @@ export const CROWDIN_PSEUDO_LOCALE = "zu-ZA";
 
 export const isInContextToolAvailable = meta.crowdinInContextToolingEnabled;
 
+const isLiveTranslationsActive =
+  typeof window !== "undefined" && window.localStorage.getItem(LIVE_TRANSLATIONS_KEY) === "enabled";
+
 export const DEFAULT_LOCALE_PROD = "en-US";
-export const DEFAULT_LOCALE = isInContextToolAvailable ? CROWDIN_PSEUDO_LOCALE : DEFAULT_LOCALE_PROD;
+export const DEFAULT_LOCALE = isInContextToolAvailable && isLiveTranslationsActive ? CROWDIN_PSEUDO_LOCALE : DEFAULT_LOCALE_PROD;
 
 export interface LanguageContextType {
   locale: string;
