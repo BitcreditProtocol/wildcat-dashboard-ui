e47d01f4d7d537a913febbc81eec9785648ccd93
JulianVIE <julian.hiess@gmail.com>
2026-07-21 13:26:56 +0200

    #381 remove duplicates

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Heading } from "@bitcredit/ui-library";
 import { FormattedMessage } from "react-intl";
 import { IdentityCard } from "@/pages/home/components/IdentityCard";
-import { ClowderCard } from "@/pages/home/components/ClowderCard";
 import { ClowderPeersCard } from "@/pages/home/components/clowder-peers/ClowderPeersCard";
 import { MintInfoCard } from "@/pages/home/components/MintInfoCard";
 
@@ -12,9 +11,8 @@ export default function HomePage() {
         <FormattedMessage id="home.page.title" defaultMessage="Home" />
       </Heading>
       <div className="flex flex-col gap-4">
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4">
           <IdentityCard />
-          <ClowderCard />
           <MintInfoCard />
         </div>
         <ClowderPeersCard />
