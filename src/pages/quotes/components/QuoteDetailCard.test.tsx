6e1935c82e6ed01a95945614a13271fb50003127
toboclaw <57799306+tobomobo@users.noreply.github.com>
2026-08-21 23:20:09 +0200

    Render real Treasury mint progress

--- src/pages/quotes/components/QuoteDetailCard.test.tsx
+++ src/pages/quotes/components/QuoteDetailCard.test.tsx
@@ -259,8 +259,8 @@ describe("QuoteDetailCard", () => {
         mintOperationStatus={{
           kid: keysetId,
           quote_id: "quote-1",
-          target: { value: 80_000_000, unit: null },
-          current: { value: 80_000_000, unit: null },
+          target: 80_000_000,
+          current: 80_000_000,
         }}
       />
     );
